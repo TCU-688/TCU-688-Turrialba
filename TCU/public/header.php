df776e4bb83faaa8e0358105a0873df160447a37
TCU-688 <106721040+TCU-688@users.noreply.github.com>
2022-06-02 19:37:43 -0600

    Update header.php

--- TCU/public/header.php
+++ TCU/public/header.php
@@ -17,25 +17,19 @@
         <link href="public/css/tablas.css" rel="stylesheet">
     </head>
     <body>
-
         <main>
-
             <section class="hero" id="hero">
                 <div class="heroText">
                     <h1 class="text-white mt-5 mb-lg-4" data-aos="zoom-in" data-aos-delay="800">
                         TC-688 Turrialba
                     </h1>
-
                 </div>
-
                 <div class="videoWrapper">
                     <video autoplay="" loop="" muted="" class="custom-video" poster="public/videos/792bd98f3e617786c850493560e11c45.jpg">
                         <source src="public/videos/814dc43e870597176cad645798825c03.mp4" type="video/mp4">
-
                         Your browser does not support the video tag.
                     </video>
                 </div>
-
                 <div class="overlay"></div>
             </section>
 
@@ -44,11 +38,9 @@
                     <a class="navbar-brand" href="index.php">
                         <strong>TC-688 Turrialba</strong>
                     </a>
-
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item active">
